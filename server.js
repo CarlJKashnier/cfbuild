071d1e80a0d69a8567fa1d5437c708a25ac2ebbb
Carl Kashnier <carlkashnier@gmail.com>
2016-08-08 19:36:45 -0400

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var MongoStore = require('connect-mongostore')(session);
 
 require('./passport.js');
 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI,{authMechanism: 'ScramSHA1'});
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
     extended: false
